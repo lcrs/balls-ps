ee34c101c062f578039170bf35314d1e16ab48ea
Lewis <lewis@beast-3.home>
2013-09-02 02:23:22 +0100

    Everything hooked up, sensitivity a bit weird

--- balls.c
+++ balls.c
@@ -11,52 +11,59 @@
 PSCryptor *cryp;
 int sock;
 int tid = 0;
-
 struct colour {
 	float r, g, b;
 } lift, gamma, gain;
+char *templ;
 
 // Send script to Photoshop
 void hi(void) {
-	const char *templ = "var idsetd = charIDToTypeID( 'setd' );\
-    var desc13 = new ActionDescriptor();\
-    var idnull = charIDToTypeID( 'null' );\
-        var ref8 = new ActionReference();\
-        var idAdjL = charIDToTypeID( 'AdjL' );\
-        var idOrdn = charIDToTypeID( 'Ordn' );\
-        var idTrgt = charIDToTypeID( 'Trgt' );\
-        ref8.putEnumerated( idAdjL, idOrdn, idTrgt );\
-    desc13.putReference( idnull, ref8 );\
-    var idT = charIDToTypeID( 'T   ' );\
-        var desc14 = new ActionDescriptor();\
-        var idpresetKind = stringIDToTypeID( 'presetKind' );\
-        var idpresetKindType = stringIDToTypeID( 'presetKindType' );\
-        var idpresetKindCustom = stringIDToTypeID( 'presetKindCustom' );\
-        desc14.putEnumerated( idpresetKind, idpresetKindType, idpresetKindCustom );\
-        var idAdjs = charIDToTypeID( 'Adjs' );\
-            var list1 = new ActionList();\
-                var desc15 = new ActionDescriptor();\
-                var idChnl = charIDToTypeID( 'Chnl' );\
-                    var ref9 = new ActionReference();\
-                    var idChnl = charIDToTypeID( 'Chnl' );\
-                    var idChnl = charIDToTypeID( 'Chnl' );\
-                    var idCmps = charIDToTypeID( 'Cmps' );\
-                    ref9.putEnumerated( idChnl, idChnl, idCmps );\
-                desc15.putReference( idChnl, ref9 );\
-                var idGmm = charIDToTypeID( 'Gmm ' );\
-                desc15.putDouble( idGmm, %f );\
-            var idLvlA = charIDToTypeID( 'LvlA' );\
-            list1.putObject( idLvlA, desc15 );\
-        desc14.putList( idAdjs, list1 );\
-    var idLvls = charIDToTypeID( 'Lvls' );\
-    desc13.putObject( idT, idLvls, desc14 );\
-executeAction( idsetd, desc13, DialogModes.NO );\
-";
 	char *js;
 	char *crypted;
 	int plainlen, cryptlen, t, w;
+	int ri0, ri1, ro0, ro1, gi0, gi1, go0, go1, bi0, bi1, bo0, bo1;
+	
+	// Oh dear
+	// Photoshop uses separate knobs when lift or gain go off the ends
+	// Here ri0 is red input 0, i.e. red input knob at black end of histogram
+	ri0 = ro0 = 0;
+	if(lift.r > 0.0) {
+		ro0 = lift.r;
+	} else {
+		ri0 = -lift.r;
+	}
+	ri1 = ro1 = 255;
+	if(gain.r < 255.0) {
+		ro1 = gain.r;
+	} else {
+		ri1 = 255.0 - (gain.r - 255.0);
+	}
+	gi0 = go0 = 0;
+	if(lift.g > 0.0) {
+		go0 = lift.g;
+	} else {
+		gi0 = -lift.g;
+	}
+	gi1 = go1 = 255;
+	if(gain.g < 255.0) {
+		go1 = gain.g;
+	} else {
+		gi1 = 255.0 - (gain.g - 255.0);
+	}
+	bi0 = bo0 = 0;
+	if(lift.b > 0.0) {
+		bo0 = lift.b;
+	} else {
+		bi0 = -lift.b;
+	}
+	bi1 = bo1 = 255;
+	if(gain.b < 255.0) {
+		bo1 = gain.b;
+	} else {
+		bi1 = 255.0 - (gain.b - 255.0);
+	}
 
-	asprintf(&js, templ, gamma.g);
+	asprintf(&js, templ, ri0, ri1, gamma.r, ro0, ro1, gi0, gi1, gamma.g, go0, go1, bi0, bi1, gamma.b, bo0, bo1);
 
 	plainlen = strlen(js) + 3 * sizeof(int);
 	cryptlen = PSCryptor::GetEncryptedLength(plainlen);
@@ -85,7 +92,6 @@ executeAction( idsetd, desc13, DialogModes.NO );\
 	// Our data
 	memcpy(crypted + 3 * sizeof(t), js, strlen(js));
 
-	// Do the stuff
 	cryp->EncryptDecrypt(true, crypted, plainlen, crypted, cryptlen, (size_t *) &cryptlen);
 	w = write(sock, crypted, cryptlen);
 
@@ -94,17 +100,24 @@ executeAction( idsetd, desc13, DialogModes.NO );\
 }
 
 int main(int argc, char **argv) {
+	// Read template .js file
+	FILE *f;
+	int len;
+	f = fopen("omg.js", "rb");
+	fseek(f, 0, SEEK_END);
+	len = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	templ = (char *) malloc(len + 1);
+	fread(templ, len, 1, f);
+
 	// Connect to Photoshop
 	struct sockaddr_in addr;
-
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addr.sin_port = htons(49494);
 	connect(sock, (struct sockaddr *) &addr, sizeof(addr));
 	fcntl(sock, F_SETFL, O_NONBLOCK);
-
-	// Setup encryption
 	cryp = new PSCryptor("popeface");
 
 	// Connect to the balls
@@ -112,7 +125,6 @@ int main(int argc, char **argv) {
 	hid_device *ball1, *ball2, *ball3;
 	unsigned char b[256];
 	int r, dirty = 0, pending = 0;
-
 	devlist = hid_enumerate(0x047d, 0x2048);
 	c = devlist;
 	ball1 = hid_open_path(c->path);
@@ -124,30 +136,65 @@ int main(int argc, char **argv) {
 	ball3 = hid_open_path(c->path);
 	hid_set_nonblocking(ball3, 1);
 
-	// Read from the balls and write to Photoshop
+	// Read from the balls, update state, write to Photoshop
+	float ring, x, y;
 	lift.r = lift.g = lift.b = 0.0;
 	gamma.r = gamma.g = gamma.b = 1.0;
-	gain.r = gain.g = gain.b = 1.0;
+	gain.r = gain.g = gain.b = 255.0;
 	while(1) {
 		r = hid_read(ball1, b, sizeof(b));
-		if(r) {
-			//printf("ball 1 %02hhx %02hhx %02hhx\n", b[1], b[2], b[3]);
+		if(r > 0) {
+			x = ((float) (signed char) b[1]) / 100.0;
+			lift.g += x;
+			lift.r -= x / 2.0;
+			lift.b -= x / 2.0;
+			y = ((float) (signed char) b[2]) / 100.0;
+			lift.r += y;
+			lift.g -= y / 2.0;
+			lift.b -= y / 2.0;
+			ring = ((float) (signed char) b[3]) / 1.0;
+			lift.r -= ring;
+			lift.g -= ring;
+			lift.b -= ring;
+			dirty = 1;
 		}
 		r = hid_read(ball2, b, sizeof(b));
-		if(r) {
-			//printf("ball 2 %02hhx %02hhx %02hhx\n", b[1], b[2], b[3]);
-			gamma.g += ((float) (signed char) b[2]) / 1000.0;
+		if(r > 0) {
+			x = ((float) (signed char) b[1]) / 1000.0;
+			gamma.g += x;
+			gamma.r -= x / 2.0;
+			gamma.b -= x / 2.0;
+			y = ((float) (signed char) b[2]) / 1000.0;
+			gamma.r += y;
+			gamma.g -= y / 2.0;
+			gamma.b -= y / 2.0;
+			ring = ((float) (signed char) b[3]) / 100.0;
+			gamma.r -= ring;
+			gamma.g -= ring;
+			gamma.b -= ring;
 			dirty = 1;
 		}
 		r = hid_read(ball3, b, sizeof(b));
-		if(r) {
-			//printf("ball 3 %02hhx %02hhx %02hhx\n", b[1], b[2], b[3]);
+		if(r > 0) {
+			x = ((float) (signed char) b[1]) / 100.0;
+			gain.g += x;
+			gain.r -= x / 2.0;
+			gain.b -= x / 2.0;
+			y = ((float) (signed char) b[2]) / 100.0;
+			gain.r += y;
+			gain.g -= y / 2.0;
+			gain.b -= y / 2.0;
+			ring = ((float) (signed char) b[3]) / 0.5;
+			gain.r -= ring;
+			gain.g -= ring;
+			gain.b -= ring;
+			dirty = 1;
 		}
 		r = read(sock, b, sizeof(b));
 		if(r > 0) {
 			pending--;
 		}
-		if(dirty && pending < 2) {
+		if(dirty && (pending < 2)) {
 			hi();
 			pending++;
 			dirty = 0;
